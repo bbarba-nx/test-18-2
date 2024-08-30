29edd99920d5dea0e327e6eefcd2c103bec65c45
Brandon Barba <brandon@nrwl.io>
2024-08-30 13:56:41 -0400

    Initial commit

---
+++ src/lib/test-18-2.spec.ts
@@ -0,0 +1,7 @@
+import { test182 } from './test-18-2';
+
+describe('test182', () => {
+  it('should work', () => {
+    expect(test182()).toEqual('test-18-2');
+  });
+});
---
+++ src/lib/test-18-2.ts
@@ -0,0 +1,3 @@
+export function test182(): string {
+  return 'test-18-2';
+}
